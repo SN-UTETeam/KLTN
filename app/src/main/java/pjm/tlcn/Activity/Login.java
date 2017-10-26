5167681be221b6d4bdcfb2f467725ed345ae1d57
Pjm <pjm.master@gmail.com>
2017-10-26 22:27:03 +0700

    Update 10:26PM 26/10/2017 Update Sign Up and Sign In

--- app/src/main/java/pjm/tlcn/Activity/Login.java
+++ app/src/main/java/pjm/tlcn/Activity/Login.java
@@ -2,17 +2,27 @@ package pjm.tlcn.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import pjm.tlcn.R;
 
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     private Button btn_login,btn_loginwithfb;
     private TextView textview_dangky;
-
+    private EditText edt_EmailLogin,edt_PassWordLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +32,32 @@ public class Login extends AppCompatActivity {
         btn_login = (Button) findViewById(R.id.btn_login);
         btn_loginwithfb = (Button) findViewById(R.id.btn_loginwithfb);
         textview_dangky = (TextView) findViewById(R.id.textview_dangky);
+        edt_EmailLogin = (EditText) findViewById(R.id.edt_EmailLogin);
+        edt_PassWordLogin = (EditText) findViewById(R.id.edt_PassWordLogin);
+        edt_PassWordLogin.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
 
+        //Create User FireBaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         //SetOnlick btn_login
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this,MainActivity.class);
-                startActivity(intent);
+                mAuth.signInWithEmailAndPassword(edt_EmailLogin.getText().toString(),edt_PassWordLogin.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Login success",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(Login.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(Login.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 
             }
         });
--- app/src/main/java/pjm/tlcn/Activity/Register.java
+++ app/src/main/java/pjm/tlcn/Activity/Register.java
@@ -1,33 +1,76 @@
 package pjm.tlcn.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import pjm.tlcn.R;
 
 public class Register extends AppCompatActivity {
-private TextView textview_registerwithEmail;
+    private static final String TAG = "Register";
+    public FirebaseAuth mAuth;
+    public TextView tv_dangnhap;
+    public Button btn_Register;
+    public EditText edt_EmailRegister,edt_PassWordRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         //Create Variable
-        textview_registerwithEmail = (TextView) findViewById(R.id.textview_registerwithEmail);
+        btn_Register = (Button) findViewById(R.id.btn_Register);
+        tv_dangnhap = (TextView) findViewById(R.id.tv_dangnhap);
+        edt_EmailRegister = (EditText) findViewById(R.id.edt_EmailRegister);
+        edt_PassWordRegister = (EditText) findViewById(R.id.edt_PassWordRegister);
+        edt_PassWordRegister.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        //Create User FireBaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
 
-        //Set Onclick textview_registerwithEmail
-        textview_registerwithEmail.setOnClickListener(new View.OnClickListener() {
+        btn_Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Register.this,RegisterWithEmail.class);
-                startActivity(intent);
+                mAuth.createUserWithEmailAndPassword(edt_EmailRegister.getText().toString(),edt_PassWordRegister.getText().toString())
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Register.this,"Đăng ký thành công!",Toast.LENGTH_LONG).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                }
+                                else {
+                                    Toast.makeText(Register.this,task.getException().toString(),Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
         });
-        //End Set Onclick textview_registerwithEmail
+
+
+
+
+
+        //Set Onclick tv_dangnhap
+        tv_dangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        //End Set Onclick tv_dangnhap
 
     }
+
 }

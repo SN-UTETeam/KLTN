39cfff279e74677b4834f395f2b2510d53302f99
thienphuit <thienphua7@gmail.com>
2017-11-04 17:32:33 +0700

    commit changed

--- app/src/main/java/pjm/tlcn/Adapter/ListViewAdapter.java
+++ app/src/main/java/pjm/tlcn/Adapter/ListViewAdapter.java
@@ -8,9 +8,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
+import pjm.tlcn.Model.User;
 import pjm.tlcn.R;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
+import static pjm.tlcn.Activity.Login.user_id;
 
 /**
  * Created by thienphu on 10/26/2017.
@@ -19,7 +32,8 @@ import pjm.tlcn.R;
 public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHolder> {
     private Activity activity;
     private List<RecyclerViewItem> items;
-
+    private DatabaseReference uDatabase;
+    private StorageReference sDatabase;
     public ListViewAdapter(Activity activity, List<RecyclerViewItem> items) {
         this.activity = activity;
         this.items = items;
@@ -34,9 +48,36 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        viewHolder.imageView.setImageResource(items.get(position).getDrawableId());
+    public void onBindViewHolder(final ViewHolder viewHolder, final int position) {
+        //Firebase
+        uDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+        sDatabase = FirebaseStorage.getInstance().getReference().child("AvatarUsers").child(user_id);
+
+       // viewHolder.imageView.setImageResource(items.get(position).getDrawableId());
         viewHolder.textView.setText(items.get(position).getName());
+
+        //Load user profile
+        uDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Picasso.with(getApplicationContext())
+                        .load(user.getAvatarurl())
+                        .fit()
+                        .centerInside()
+                        .into(viewHolder.imname);
+             //   edt_username_editprofile.setText(user.getUsername());
+              //  edt_describer_editprofile.setText(user.getDescriber());
+             //   edt_email_editprofile.setText(user.getEmail());
+             //   if(!user.getPhonenumber().equals(null)) edt_phonenumber_editprofile.setText(user.getPhonenumber());
+               // else edt_phonenumber_editprofile.setText("");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -48,12 +89,13 @@ public class ListViewAdapter extends RecyclerView.Adapter<ListViewAdapter.ViewHo
      * View holder to display each RecylerView item
      */
     protected class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView imageView;
+        private ImageView imageView,imname;
         private TextView textView;
 
         public ViewHolder(View view) {
             super(view);
             imageView = (ImageView) view.findViewById(R.id.image);
+            imname = (ImageView) view.findViewById(R.id.idname);
             textView = (TextView)view.findViewById(R.id.text);
         }
 

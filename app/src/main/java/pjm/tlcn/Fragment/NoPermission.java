7b7e86ecbc2d43e2e9c41d5df4ea4df09465f58c
Pjm <pjm.master@gmail.com>
2017-11-12 23:01:58 +0700

    Update Toolbar

---
+++ app/src/main/java/pjm/tlcn/Fragment/NoPermission.java
@@ -0,0 +1,21 @@
+package pjm.tlcn.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pjm.tlcn.R;
+
+
+public class NoPermission extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_no_permission, container, false);
+    }
+
+}
